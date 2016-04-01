322a36692395fb4dc150bc039d9a0241289eeead
grig0r <grig0r@vaio.localdomain>
2016-04-02 00:41:11 +0200

    conf

---
+++ .scripts/yaourt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz  # download source tarball
+tar xfz package-query.tar.gz  # unpack tarball
+cd package-query  &&  makepkg -sr  # cd and create package from source
+sudo pacman -U package-query*.pkg.tar.xz  # install package - need root privileges
+
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+tar xzf yaourt.tar.gz
+cd yaourt  &&  makepkg -sr
+sudo pacman -U yaourt*.pkg.tar.xz
